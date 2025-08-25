297ca9bcdf26133d7525cacbd4f0c2abd7b4baaa
hakkman2 <hakkman2@gmail.com>
2025-08-25 11:32:29 +0100

    new

--- hello_world.py
+++ hello_world.py
@@ -37,34 +37,57 @@
 # # print(days.get(day,"Enter number from 1 to 5."))
 
 import time
-number = [1, 3, 7, 2, 4, 90, 87, 6, 54, 22, 23, 24, 25]
+# number = [1, 3, 7, 2, 4, 90, 87, 6, 54, 22, 23, 24, 25]
 
-for num in range(len(number)):
-    if number[num] % 2 == 0:
-        time.sleep(0.5)
-        print("even -->", num, number[num])
+# for num in range(len(number)):
+#     if number[num] % 2 == 0:
+#         time.sleep(0.5)
+#         print("even -->", num, number[num])
 
-for num in range(len(number)):
-    if number[num] % 2 != 0:
-        time.sleep(0.5)
-        print("odd -->", num, number[num])
+# for num in range(len(number)):
+#     if number[num] % 2 != 0:
+#         time.sleep(0.5)
+#         print("odd -->", num, number[num])
 
-while True:
-    command = input("enter a command: ")
-    if command == "e":
-        break
-c = 0
-while c != 100:
-    c += 1
-    print(c)
-    if c == 100:
-        break
+# while True:
+#     command = input("enter a command: ")
+#     if command == "e":
+#         break
+# c = 0
+# while c != 100:
+#     c += 1
+#     print(c)
+#     if c == 100:
+#         break
 
-even = 0
+# even = 0
 
-while even != 1000:
-        even += 1
-        if even % 2 == 0:
-            print("Even -->", even)
-        pass
+# while even != 1000:
+#         even += 1
+#         if even % 2 == 0:
+#             print("Even -->", even)
+#         pass
+
+# number = [1,56,234,87,4,76,24,69,90,135]
+# def is_even(num):
+#     for num in range(len(number)):
+#         if number[num] % 2 == 0:
+#             print("even --> ", number[num])
+# is_even(5)
+
+
+try:
+    takenum = int(input("Enter a number from 1-100000: "))
+
+    def is_even(takenum):
+        if takenum % 2 == 0:
+            print("even")
+        elif takenum % 2 != 0:
+            print("Old")
+    is_even(takenum)
+except ValueError:
+    print("try again!!!")
+
+    
+
     
--- llmglobal.py
+++ llmglobal.py
@@ -2,17 +2,15 @@ import os
 # pip install openai
 from openai import OpenAI
 
-api_key1 = "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"
-api_key2 = "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"
-api_key3 = "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"
-
+api_key1 = ""
+  
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key1,
 )
 # model parameters
 system_prompt = "You are an expert Economics tutor. Answer all questions with clear explanations and real-world examples."
-query = "Explain the law of demand with an example."
+query = "How are you?"
 temperature = 0.4
 model = "google/gemini-2.5-flash"
 stream = True
