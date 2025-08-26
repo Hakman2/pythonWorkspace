063d992ff7d0b2bce824ce48bb52fbbfc6202b8a
hakkman2 <hakkman2@gmail.com>
2025-08-26 08:01:32 +0100

    chcked

--- shapes/text_utils.py
+++ shapes/text_utils.py
@@ -2,9 +2,3 @@ def is_palindrome(text):
     # make lowercase & remove spaces
     cleaned = text.replace(" ", "").lower()
     return cleaned == cleaned[::-1]
-
-# Examples
-print(is_palindrome("madam"))       # True
-print(is_palindrome("racecar"))     # True
-print(is_palindrome("hello"))       # False
-print(is_palindrome("Nurses run"))  # True
